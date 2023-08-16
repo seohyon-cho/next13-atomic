0709c3d979c3e1e99b7a6a9d8e84977c7799dcad
tubi55 <tubi55@nate.com>
2023-08-16 11:36:08 +0900

    size속성 지정으로 초기 이미지 용량 최적화

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,10 @@ export default function Home({ meals }) {
 									alt={item.strMeal}
 									priority
 									fill
-									quality={50}
+									quality={100}
 									style={{ objectFit: 'cover' }}
+									//이미지 속성이 fill로 지정되어 있을경우 frame크기에 상관없이 초기 로딩시 전체 브라우저 크기의 100vw크기의 용량으로 가져오기 때문에 브라우저 폭에 출력될 크기를 지정해서 이미지 성능 향상
+									sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 								/>
 							</div>
 						))}
