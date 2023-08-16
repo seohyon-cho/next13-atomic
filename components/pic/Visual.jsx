758bea90b7fab65525b18de57b844388521764aa
tubi55 <tubi55@nate.com>
2023-08-16 13:00:24 +0900

    자식요소를 활용한 커스텀 컴포넌트 제작

--- components/pic/Visual.jsx
+++ components/pic/Visual.jsx
@@ -5,7 +5,13 @@ import styles from './Visual.module.scss';
 export function Visual({ imgSrc, style }) {
 	return (
 		<div className={clsx(styles.pic)} style={style}>
-			<Image src={imgSrc} alt={imgSrc} priority fill sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' />
+			<Image
+				src={imgSrc}
+				alt={imgSrc}
+				priority
+				fill
+				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+			/>
 		</div>
 	);
 }
@@ -13,8 +19,29 @@ export function Visual({ imgSrc, style }) {
 export function VisualWithText({ imgSrc, style, imgTxt }) {
 	return (
 		<div className={clsx(styles.picWithTxt)} style={style}>
-			<Image src={imgSrc} alt={imgSrc} priority fill sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' />
+			<Image
+				src={imgSrc}
+				alt={imgSrc}
+				priority
+				fill
+				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+			/>
 			<h2>{imgTxt}</h2>
+		</div>
+	);
+}
+
+export function VisualWithContent({ imgSrc, style, children }) {
+	return (
+		<div className={clsx(styles.picWithContent)} style={style}>
+			<Image
+				src={imgSrc}
+				alt={imgSrc}
+				priority
+				fill
+				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+			/>
+			{children}
 		</div>
 	);
 }
