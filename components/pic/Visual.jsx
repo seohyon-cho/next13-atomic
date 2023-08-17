0df886c3f6322cff07d592141e2d62953f340f63
tubi55 <tubi55@nate.com>
2023-08-17 10:05:11 +0900

    부모요소에서 적용된 클래스명을 자식 아톰 컴포넌트의 props로 전달

--- components/pic/Visual.jsx
+++ components/pic/Visual.jsx
@@ -2,13 +2,21 @@ import Image from 'next/image';
 import clsx from 'clsx';
 import styles from './Visual.module.scss';
 
-export function Visual({ imgSrc, style, imgTxt, children }) {
+export function Visual({
+	imgSrc,
+	style,
+	imgTxt,
+	children,
+	//해당 아톰 컴포넌트가 호출되는 위치에서의 className props를 내부로 전달
+	className,
+	priority = false,
+}) {
 	return (
-		<div className={clsx(styles.pic)} style={style}>
+		<div className={clsx(styles.pic, className)} style={style}>
 			<Image
 				src={imgSrc}
 				alt={imgSrc}
-				priority
+				priority={priority}
 				fill
 				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 			/>
