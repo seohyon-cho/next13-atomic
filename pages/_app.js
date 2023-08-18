868aa145599ab228f7f24d1a0df516d0202b1bc1
tubi55 <tubi55@nate.com>
2023-08-18 12:13:25 +0900

    next에서 컴포넌트 랜더링 흐름

--- pages/_app.js
+++ pages/_app.js
@@ -11,3 +11,11 @@ export default function App({ Component, pageProps }) {
 		</Layout>
 	);
 }
+
+/*
+  컴포넌트 렌더링 흐름
+  1. _app.js에서 공통의 layout 템플릿 컴포넌트를 가져와서 전체 Component를 Wrapping 
+  2. _app.js에 있는 Component는 page폴더 안쪽에 있는 각각의 페이지 컴포넌트를 의미
+  3. 모든 페이지 컴포넌트에는 Layout컴포넌트의 공통의 구조가 적용됨
+  4. 각각의 페이지 컴포넌트에서 페이지별로 들어각 컨텐츠 추가
+*/
