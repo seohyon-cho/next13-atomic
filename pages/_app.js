4de54e03c5616babdd67e3b8646afda029775fc9
tubi55 <tubi55@nate.com>
2023-08-18 12:37:29 +0900

    폴더별로 컴포넌트 그룹화

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from '@/components/template/Layout';
+import Layout from '@/components/template/Layout/Layout';
 import '@/styles/globals.scss';
 import axios from 'axios';
 
@@ -17,5 +17,5 @@ export default function App({ Component, pageProps }) {
   1. _app.js에서 공통의 layout 템플릿 컴포넌트를 가져와서 전체 Component를 Wrapping 
   2. _app.js에 있는 Component는 page폴더 안쪽에 있는 각각의 페이지 컴포넌트를 의미
   3. 모든 페이지 컴포넌트에는 Layout컴포넌트의 공통의 구조가 적용됨
-  4. 각각의 페이지 컴포넌트에서 페이지별로 들어각 컨텐츠 추가
+  4. 각각의 페이지 컴포넌트에서 페이지별로 들어갈 컨텐츠 추가
 */
