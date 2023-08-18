00afcc1ec1cb361afa46948974d3b72c1483c4c0
tubi55 <tubi55@nate.com>
2023-08-18 10:11:47 +0900

    로딩바 스타일링

--- components/atoms/pic/Pic.jsx
+++ components/atoms/pic/Pic.jsx
@@ -2,7 +2,9 @@ import Image from 'next/image';
 import clsx from 'clsx';
 import styles from './Pic.module.scss';
 import Link from 'next/link';
+import { HashLoader } from 'react-spinners';
 
+//react-spinners
 export function Pic({
 	imgSrc,
 	style,
@@ -22,12 +24,9 @@ export function Pic({
 				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 			/>
 
-			{/* 컴포넌트 호출시 전달되는 prop유무에 따라서 반환하는 JSX분기처리 */}
 			{imgTxt && (
 				<>
-					{/* 이미지위에 글자가 출력되야 되므로 dimmed처리할 마스크 */}
 					<aside></aside>
-					{/* url 값이 전달되면 Link컴포넌트를 연결해서 출력 */}
 					{url ? (
 						<h2>
 							<Link href={url}>{imgTxt}</Link>
@@ -43,6 +42,18 @@ export function Pic({
 					{url ? <Link href={url}>children</Link> : children}
 				</>
 			)}
+
+			{/* spinner로딩 */}
+			<HashLoader
+				cssOverride={{
+					position: 'absolute',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
+				}}
+				size={100}
+				color={'aqua'}
+			/>
 		</div>
 	);
 }
