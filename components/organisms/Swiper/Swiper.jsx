504fdbca4ff2899f50806c011b5ae386f953a2b0
tubi55 <tubi55@nate.com>
2023-08-21 11:40:50 +0900

    Autoplay 설정

--- components/organisms/Swiper/Swiper.jsx
+++ components/organisms/Swiper/Swiper.jsx
@@ -5,6 +5,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Autoplay } from 'swiper';
 import 'swiper/css';
 
+//Next에서는 Autopaly, Pagination, Navigation기능을 활성화하기 위해 SwiperCore.use 사용
+SwiperCore.use([Autoplay]);
+
 //npm i swiper@9
 function SwiperWrap({ recipe, category }) {
 	console.log('recipe', recipe);
@@ -16,12 +19,27 @@ function SwiperWrap({ recipe, category }) {
 		<figure className={clsx(styles.visual)}>
 			<Title style={{ position: 'absolute', top: '20vh', left: '10vw', fontSize: 50, color: 'orange' }}>{category}</Title>
 
-			<Swiper className={clsx(styles.swiper)} loop={true} grabCursor={true} slidesPerView={3} spaceBetween={100} centeredSlides={true}>
+			<Swiper
+				className={clsx(styles.swiper)}
+				modules={[Autoplay]}
+				autoplay={{ delay: 2000, disableOnInteraction: true }}
+				loop={true}
+				grabCursor={true}
+				slidesPerView={1}
+				spaceBetween={100}
+				centeredSlides={true}
+				breakpoints={{
+					1200: {
+						slidesPerView: 3,
+						spaceBetween: 50,
+					},
+				}}
+			>
 				{recipe.map((item) => (
 					<SwiperSlide key={item.idMeal} className={clsx(styles.swiperSlide)}>
 						<div>
 							<Title tag={'h3'} url={'/'} type={'slogan'}>
-								{item.strMeal}
+								{item.strMeal.length > 25 ? item.strMeal.substr(0, 25) : item.strMeal}
 							</Title>
 						</div>
 					</SwiperSlide>
