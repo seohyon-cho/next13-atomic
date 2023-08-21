708ab8f8d7992c4508830271fea263b3bcb07a9d
tubi55 <tubi55@nate.com>
2023-08-21 09:04:28 +0900

    코드 정리 (140)

--- components/atoms/text/Text.jsx
+++ components/atoms/text/Text.jsx
@@ -18,48 +18,27 @@ const orbitron = Orbitron({
 	variable: '--font-orbitron',
 });
 
-function Text({
-	children,
-	url,
-	style,
-	className,
-	type,
-	tag = 'p',
-	isOn = false,
-}) {
-	//next/useRouter훅으로부터 객체를 반환받고
+function Text({ children, url, style, className, type, tag = 'p', isOn = false }) {
 	const router = useRouter();
-	//해당객체에 있는 pathname값 가져오기 (현재 활성화되어있는 라우터명)
 	const currentPath = router.pathname;
 
-	//컴포넌트 이름, 디폴트 태그명
 	return React.createElement(
 		tag,
 		{
 			className: clsx(
-				//현재라우터명과 url로 가져온 라우터명이 동일하면 on클래스 추가
 				currentPath === url ? styles.on : '',
-				styles.txt, //클래스명
+				styles.txt,
 				className,
 				nanum.variable,
 				orbitron.variable,
-				styles[`txt_${type}`], //추가 클래스명
-				isOn && styles.on //전달되는 boolean값에 따라 고유클래스 on추가, module.sass가 자체적으로 고유클래스명으로 변환하기 때문에 부모의 클래스명을 내부 전용 css에 연결하는게 불가
+				styles[`txt_${type}`],
+				isOn && styles.on
 			),
 			style: url ? style : { ...style, transitionDuration: '0.5s' },
 			onMouseEnter: (e) => (e.target.style.color = style?.hoverColor),
 			onMouseLeave: (e) => (e.target.style.color = style?.color),
 		},
-		url
-			? React.createElement(
-					Link,
-					{
-						href: url,
-						style: { transitionDuration: '0.5s' },
-					},
-					children
-			  )
-			: children
+		url ? React.createElement(Link, { href: url, style: { transitionDuration: '0.5s' } }, children) : children
 	);
 }
 
--- pages/index.js
+++ pages/index.js
@@ -24,15 +24,11 @@ export async function getStaticProps() {
 	const { data: obj } = await axios.get('/categories.php');
 	const items = obj.categories;
 	items.forEach((el) => list.push(el.strCategory));
-	const newList = list.filter(
-		(el) => el !== 'Goat' && el !== 'Vegan' && el !== 'Starter'
-	);
+	const newList = list.filter((el) => el !== 'Goat' && el !== 'Vegan' && el !== 'Starter');
 
 	const randomNum = Math.floor(Math.random() * newList.length);
 
-	const { data } = await axios.get(
-		`/filter.php?c=${newList[randomNum]}`
-	);
+	const { data } = await axios.get(`/filter.php?c=${newList[randomNum]}`);
 
 	return {
 		props: { ...data, category: newList[randomNum] },
