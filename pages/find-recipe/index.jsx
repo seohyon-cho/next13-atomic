46e4be3deb1ec172ccefea66a29c123470bae488
tubi55 <tubi55@nate.com>
2023-08-22 11:56:00 +0900

    리액트쿼리 커스텀훅 변경요청 이벤트 방법

--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -3,13 +3,14 @@ import styles from './style.module.scss';
 import axios from 'axios';
 import Category from '@/components/molecules/Category/Category';
 import { useRecipeByCategory } from '@/hooks/useRecipe';
+import { useState } from 'react';
 
 export default function Recipe({ categories }) {
-	//useRecipeByCategory (자주활용되는 리액트 기능을 패키징해서 hook형태로 만들어놓은 함수)
-	//인수가 전달되면 해당값을 활용해서 react-qeury를 이용하여 비동기서버데이터를 호출하고 해당 결과값을 객체형태로 리턴하는 함수
-	//react-query를 굳이 써야되는 이유 : 반환받은 서버 데이터를 캐싱처리해서 동일한 데이터 요청시 다시 refetching하지 않기 위함
-	// {data(서버데이터), isSuccess(요청성공시 true반환), isError(요청실패시 true반환),isLoading(요청중일떄 true반환), refetch(강제 refetching함수)}
-	const { data, isSuccess } = useRecipeByCategory('Beef');
+	//react-query를 활용하는 쿼리키 인수값을 State에 담음
+	const [Selected, setSelected] = useState(categories[0].strCategory);
+	//해당 State값이 바뀔때마다 react-query훅이 호출되면서 새로운 데이터 패칭
+	const { data, isSuccess } = useRecipeByCategory(Selected);
+
 	console.log(data);
 
 	return (
@@ -19,7 +20,8 @@ export default function Recipe({ categories }) {
 			</Head>
 
 			<section className={styles.recipePage}>
-				<Category items={categories} />
+				{/* 이벤트가 발생해야 되는 자식요소에 queryKey로 연동되어 있는 state변경함수를 전달 */}
+				<Category items={categories} handler={setSelected} />
 			</section>
 		</>
 	);
