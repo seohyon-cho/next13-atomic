d673717acbc3962ad574a3262e104a77ee8da9ac
tubi55 <tubi55@nate.com>
2023-08-22 10:21:50 +0900

    기본 버튼 컴포넌트 추가 및 스타일링

--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -1,14 +1,30 @@
 import Head from 'next/head';
 import styles from './style.module.scss';
+import axios from 'axios';
 
-export default function Recipe() {
+export default function Recipe({ categories }) {
+	console.log(categories);
 	return (
 		<>
 			<Head>
 				<title>Recipe Page</title>
 			</Head>
 
-			<div className={styles.box}></div>
+			<section className={styles.recipePage}>
+				<nav>
+					{categories.map((el) => (
+						<button key={el.idCategory}>{el.strCategory}</button>
+					))}
+				</nav>
+			</section>
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const { data } = await axios.get('/categories.php');
+
+	return {
+		props: { categories: data.categories },
+	};
 }
