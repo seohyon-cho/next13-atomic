04d0a7a2867b97dee4787b0d1bebf39a953293ec
tubi55 <tubi55@nate.com>
2023-08-23 11:46:34 +0900

    페이지 전환효과 설정

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,22 @@ export default function App({ Component, pageProps }) {
 					<Layout>
 						<Component {...pageProps} />
 					</Layout>
+					{/* 페이지가 바뀌기 시작할때 나타날 프레임 */}
+					<motion.div
+						className='in'
+						initial={{ scaleX: 0 }}
+						animate={{ scaleX: 0 }}
+						exit={{ scaleX: 1 }}
+						transition={{ duration: 0.7, ease: [0.25, 0.1, 0.03, 0.99] }}
+					></motion.div>
+					{/* 페이지가 바뀌고 나서 사라질 프레임 */}
+					<motion.div
+						className='out'
+						initial={{ scaleX: 1 }}
+						animate={{ scaleX: 0 }}
+						exit={{ scaleX: 0 }}
+						transition={{ duration: 0.7, ease: [0.25, 0.1, 0.03, 0.99] }}
+					></motion.div>
 				</motion.div>
 			</AnimatePresence>
 			<ReactQueryDevtools />
