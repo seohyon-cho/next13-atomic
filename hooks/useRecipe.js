51ab1ee01a5a8f11214817bf5abe6041ba57d756
tubi55 <tubi55@nate.com>
2023-08-23 11:55:25 +0900

    자주바뀌지 않는 서버데이터의 경우는 stale, cache타임을 길게 지정해서 동일한 데이터 refetching금지

--- hooks/useRecipe.js
+++ hooks/useRecipe.js
@@ -10,8 +10,8 @@ export const useRecipeByCategory = (SelectedCategory) => {
 	return useQuery(['RecipeByCategory', SelectedCategory], getRecipeByCategory, {
 		refetchOnMount: false,
 		refetchOnWindowFocus: false,
-		cacheTime: 0,
-		staleTime: 0,
+		cacheTime: 1000 * 60 * 60 * 24,
+		staleTime: 1000 * 60 * 60 * 24,
 		retry: 3, //데이터 요청 시도 횟수(디폴트3, 네트워트상황이 안좋을때 재시도횟수 늘림)
 		//enabled값에는 truthy, falsy값이 적용안됨 (직접 boolean값을 생성해서 지정)
 		//지금 상황에서는 SSG방식으로 초기 데이터를 호출하고 있기 때문에 아래 구문을 지정안해도 잘 동작됨
