909d1c89e3d49ccecec8b8d09c822cf1f19fef5c
tubi55 <tubi55@nate.com>
2023-08-25 11:43:34 +0900

    레서피 제료 구조화해서 리턴

--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -12,6 +12,23 @@ function Detail() {
 	const { data, isSuccess } = useRecipeById(id);
 	console.log(isSuccess && data);
 
+	if (isSuccess) {
+		const keys = Object.keys(data);
+		//레시피 정보 객체에서 strIngredient문자로 시작하는 키값만 배열로 뽑음
+		const filterKeys1 = keys.filter((key) => key.startsWith('strIngredient'));
+		console.log(filterKeys1);
+		//위에서 뽑은 키값에서 value값이 빈문자거나 null인 것은 제외
+		const filterKeys2 = filterKeys1.filter((key) => data[key] !== '' && data[key] !== null);
+		console.log(filterKeys2);
+		//위에서 뽑은 키값으로 재료순서, 재료명, 재료량을 객체로 변환해서 다시 배열로 반환
+		const ingredients = filterKeys2.map((key, idx) => ({
+			index: idx + 1,
+			ingredient: data[key],
+			measuer: data[`strMeasure${idx + 1}`],
+		}));
+		console.log(ingredients);
+	}
+
 	return (
 		<section className={clsx(styles.detail)}>
 			<BounceLoader
