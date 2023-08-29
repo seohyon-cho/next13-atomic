f6dd9f6951d912c3d5bd212a2c135e94cfe2e4b1
tubi55 <tubi55@nate.com>
2023-08-29 12:55:22 +0900

    즐겨찾기 페이지 마운트시 로컬저장소의 즐겨찾기 된 레시피 아이디 확인

--- pages/my-favorait/index.jsx
+++ pages/my-favorait/index.jsx
@@ -2,8 +2,23 @@ import Head from 'next/head';
 import clsx from 'clsx';
 import { Title } from '@/components/atoms/text/Title';
 import styles from './favorait.module.scss';
+import { useState, useEffect } from 'react';
 
 function Favorait() {
+	const [SavedId, setSavedId] = useState([]);
+
+	useEffect(() => {
+		if (localStorage.getItem('savedRecipe')) {
+			setSavedId(JSON.parse(localStorage.getItem('savedRecipe')));
+		} else {
+			JSON.stringify(localStorage.setItem('savedRecipe', []));
+		}
+	}, []);
+
+	useEffect(() => {
+		console.log(SavedId);
+	}, [SavedId]);
+
 	return (
 		<>
 			<Head>
