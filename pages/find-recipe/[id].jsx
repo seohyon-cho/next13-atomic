81689d38d3e86d6718b6219cb5fe9a0168388711
tubi55 <tubi55@nate.com>
2023-08-29 10:52:53 +0900

    다이나믹 라우터에 페이지 트랜지션 모션적용시 페이지 변경될떄 prop오류 뜨는 이유와 해결방법

--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -43,6 +43,10 @@ function Detail() {
 	return (
 		<section className='detail'>
 			<BounceLoader
+				//기본적으로 next는 라우터명이 변경될떄마다 언마운트되는 페이지 컴포넌트의 csr방식으로 가져온 데이터와 스타일 노드를 제거
+				//page transition이 적용되어 있기 때문에 상세페이지에서 다른페이지로 넘어갈때 데이터는 이미 사라졌음에도 불구하고 데이터를 활용하는 컴포넌트가 계속 있으면 prop오류 발생
+				//해결방법: csr방식으로 가져오는 데이터 자체를 컴포넌트 렌더링의 조건설정
+				//데이터없으면 로딩바 출력, 데이터가 있으면 그 데이터를 활용하는 컴포넌트 출력
 				loading={!data}
 				cssOverride={{
 					position: 'absolute',
