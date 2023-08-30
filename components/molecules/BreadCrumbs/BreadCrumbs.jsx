172f61ede7051d77e28a6fe2735df54e7e22fc51
tubi55 <tubi55@nate.com>
2023-08-30 12:01:56 +0900

    -으로 붙어있는 문자열을 빈칸으로 분리하고 첫 단어를 대문자로 변경

--- components/molecules/BreadCrumbs/BreadCrumbs.jsx
+++ components/molecules/BreadCrumbs/BreadCrumbs.jsx
@@ -7,26 +7,36 @@ function BreadCrumbs({ data }) {
 	return (
 		<nav className={clsx(styles.breadcrumbs)}>
 			{data.map((name, idx) => {
-				//현재 반복되는 메뉴 순번이 마지막이 아닐떄
-				//뒤에 슬러시 붙이고 링크 추가
+				//result = -로 연결되어 있는 문자값을 빈칸으로 치환하고 각 단어의 앞글자만 대문자로 리턴
+				//find-recipe = Find Recipe
+				const result = name.includes('-')
+					? name
+							// -으로 문자열 분리
+							.split('-')
+							//분리된 문자열을 다시 첫번째만 잘라서 대문자 변경 + 첫문자만 제외한 나머지 문자값 반환해서 하나로 붙이기
+							.map((txt) => txt.charAt(0).toUpperCase() + txt.slice(1))
+							.join(' ')
+					: name;
+
+				//마지막 배열 문자값이 아닐때
 				if (idx !== data.length - 1) {
 					return (
 						<React.Fragment key={idx}>
+							{/* 텍스트가 없을떄 home으로 치환 있으면 위에서 가공한 result적용 */}
 							<Text tag={'em'} url={`/${name}`}>
-								{!name ? 'Home' : name}
+								{!name ? 'Home' : result}
 							</Text>
 							<span> / </span>
 						</React.Fragment>
 					);
-					//마지막 순번이면 글자만 생성
-					//마지막데이터에서 쿼리스트링값이 있으면(=이 있으면) 뒤에 레시피 아이디가 아닌 레시피명을 출력
-					//빈칸처리되는 정규표현식을 빈칸으로 대체(%20 -> ' ')해서 문자열 출력
+					//현재 배열 마지막 문자값이면
 				} else {
 					return (
 						<Text key={idx} tag={'strong'}>
-							{name.includes('=')
-								? name.split('=')[1].replaceAll('%20', ' ')
-								: name}
+							{/* 가공된 result을 바로 활용해서 해당 result값에서 =있으면 가공처리 없으면 그냥 result값 활용 */}
+							{result.includes('=')
+								? result.split('=')[1].replaceAll('%20', ' ')
+								: result}
 						</Text>
 					);
 				}
