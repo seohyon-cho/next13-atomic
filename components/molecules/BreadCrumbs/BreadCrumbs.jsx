9369aa76f0bbc28f8e574b54eec5dec6fa76c767
tubi55 <tubi55@nate.com>
2023-08-30 11:02:00 +0900

    쿼리값을 이용해서 레시피아이디가 아닌 레시피명을 출력

--- components/molecules/BreadCrumbs/BreadCrumbs.jsx
+++ components/molecules/BreadCrumbs/BreadCrumbs.jsx
@@ -19,10 +19,14 @@ function BreadCrumbs({ data }) {
 						</React.Fragment>
 					);
 					//마지막 순번이면 글자만 생성
+					//마지막데이터에서 쿼리스트링값이 있으면(=이 있으면) 뒤에 레시피 아이디가 아닌 레시피명을 출력
+					//빈칸처리되는 정규표현식을 빈칸으로 대체(%20 -> ' ')해서 문자열 출력
 				} else {
 					return (
 						<Text key={idx} tag={'strong'}>
-							{name}
+							{name.includes('=')
+								? name.split('=')[1].replaceAll('%20', ' ')
+								: name}
 						</Text>
 					);
 				}
