635aaeabd30ee97f89822212e405f06c8fe94f08
tubi55 <tubi55@nate.com>
2023-08-31 10:53:04 +0900

    버튼 클릭시 실시간으로 테마 색상 변경

--- components/organisms/Footer/Footer.jsx
+++ components/organisms/Footer/Footer.jsx
@@ -3,14 +3,18 @@ import clsx from 'clsx';
 import { Text } from '@/components/atoms/text/Text';
 import { BounceLoader } from 'react-spinners';
 import Btn from '@/components/atoms/Button/Btn';
+import { useGlobalData } from '@/hooks/useGlobalContext';
 
 function Footer() {
+	const { setTheme } = useGlobalData();
 	return (
 		<footer className={clsx(styles.footer)}>
 			<nav>
-				<Btn>Orange</Btn>
-				<Btn>Hotpink</Btn>
-				<Btn>Aqua</Btn>
+				{['Orange', 'Aqua', 'Hotpink'].map((el, idx) => (
+					<Btn key={idx} onClick={() => setTheme(`theme${idx + 1}`)}>
+						{el}
+					</Btn>
+				))}
 			</nav>
 			<Text type={'util'} style={{ letterSpacing: 2 }}>
 				2023 Decodelab All rights reserved.
