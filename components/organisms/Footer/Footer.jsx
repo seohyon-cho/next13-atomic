bcf4dd9bf9127af2acf12de642a8f16509e2c751
tubi55 <tubi55@nate.com>
2023-08-31 12:36:52 +0900

    카테고리 컴포넌트 활용해서 테마변경 메뉴 출력

--- components/organisms/Footer/Footer.jsx
+++ components/organisms/Footer/Footer.jsx
@@ -4,17 +4,18 @@ import { Text } from '@/components/atoms/text/Text';
 import { BounceLoader } from 'react-spinners';
 import Btn from '@/components/atoms/Button/Btn';
 import { useGlobalData } from '@/hooks/useGlobalContext';
+import Category from '@/components/molecules/Category/Category';
 
 function Footer() {
-	const { setTheme } = useGlobalData();
+	const { setTheme, Theme } = useGlobalData();
 	return (
 		<footer className={clsx(styles.footer)}>
 			<nav>
-				{['Orange', 'Aqua', 'Hotpink'].map((el, idx) => (
-					<Btn key={idx} onClick={() => setTheme(`theme${idx + 1}`)}>
-						{el}
-					</Btn>
-				))}
+				<Category
+					items={['theme1', 'theme2', 'theme3']}
+					active={Theme}
+					onClick={setTheme}
+				/>
 			</nav>
 			<Text type={'util'} style={{ letterSpacing: 2 }}>
 				2023 Decodelab All rights reserved.
