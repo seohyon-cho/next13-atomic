06a3f2784085e478f656defe38738853a2e12bf1
seohyon-cho <flanique@naver.com>
2024-01-22 14:50:33 +0900

    템플릿 폴더

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,7 @@
-import Layout from '@/components/template/Layout/Layout';
-import '@/styles/globals.scss';
-import '@/styles/theme.scss';
-import axios from 'axios';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { keepStyle } from '@/libs/keepStyle';
-import { GlobalProvider } from '@/hooks/useGlobalContext';
-keepStyle(3000);
-
-axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
-
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }) {
 	return (
-		<GlobalProvider>
-			<QueryClientProvider client={queryClient}>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-				<ReactQueryDevtools />
-			</QueryClientProvider>
-		</GlobalProvider>
+		<div>
+			<Component {...pageProps} />
+		</div>
 	);
 }
