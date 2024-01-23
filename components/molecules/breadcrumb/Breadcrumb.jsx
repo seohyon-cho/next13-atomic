e4e13157fc78fd361cac2eb1a1dd4505d4d2b93a
seohyon-cho <flanique@naver.com>
2024-01-23 15:50:56 +0900

    breadcrumb 내부에서 자체적으로 텍스트 가공

--- components/molecules/breadcrumb/Breadcrumb.jsx
+++ components/molecules/breadcrumb/Breadcrumb.jsx
@@ -7,22 +7,39 @@ import { useRouter } from 'next/router';
 export default function Breadcrumb() {
 	const pathArr = useRouter().asPath.split('/');
 
+	// 변환할 문자원본 (txt) 과 제거할 특수문자 (spc)를 인수로 받아서, 특수문자를 제거한 뒤 Capitalize하여 반환하는 함수 (화면에 출력될 메뉴 카테고리명에 활용)
+
+	// 기존에 만들어뒀던 useText 커스텀 훅을 사용하지 않는 이유 : atomic pattern에서는 커스텀 훅을 사용해야 할 경우 부분 컴포넌트가 아닌, 페이지 컴포넌트에서만 활용하는 것이 바람직함.
+	const customText = (txt, spc) => {
+		txt = txt.includes(spc)
+			? txt
+					.split(spc)
+					.map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+					.join(' ')
+			: txt;
+		return txt;
+	};
+
 	return (
 		<nav className={clsx(styles.breadcrumb)}>
 			{pathArr.map((name, idx) => {
+				// name 자체를 바꿔버리면 라우터 경로에도 문제가 생겨서, name값 자체는 변화를 주지 않고
+				// 화면에 출력될 이름 글자 자체만 displayName으로 따로 정의해서 디스플레이용으로 사용.
+				// (해당 값은 디스플레이 출력 용도로만 활용하고, 값이 바뀌는 것이므로 연산하는 로직에는 활용하면 안 됨.)
+				const displayName = customText(name, '-');
 				if (idx === pathArr.length - 1) {
 					return (
-						<Text key={idx} tagName={'span'} isOn>
-							{name}
+						<Text key={idx} tagName={'strong'} isOn>
+							{displayName}
 						</Text>
 					);
 				} else {
 					return (
 						<React.Fragment key={idx}>
-							<Text tagName={'span'} url={'/' + name}>
-								{name === '' ? 'Home' : name}
-							</Text>{' '}
-							<span>/</span>
+							<Text tagName={'em'} url={'/' + name}>
+								{name === '' ? 'Home' : displayName}
+							</Text>
+							<span> / </span>
 						</React.Fragment>
 					);
 				}
