8940dd907a38ffaffedb3f7aa63e8d88091f9f58
seohyon-cho <flanique@naver.com>
2024-01-23 12:36:55 +0900

    data를 전달 받아서, 해당 data가 연동된 table 컴포넌트 추가 및 출력

--- components/atoms/table/Table.jsx
+++ components/atoms/table/Table.jsx
@@ -11,7 +11,24 @@ export default function Table({ data, title, className }) {
 				</Text>
 			)}
 
-			<table className={clsx(styles.table, className)}></table>
+			<table className={clsx(styles.table, className)} border={1}>
+				<thead>
+					<tr>
+						{Object.keys(data[0]).map((key) => (
+							<th key={key}>{key}</th>
+						))}
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((el, idx) => (
+						<tr key={idx}>
+							{Object.values(el).map((val) => (
+								<td key={idx}>{val}</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	);
 }
