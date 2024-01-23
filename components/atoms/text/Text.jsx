8121bc9e51e5d9a606941c9352aef6c0e6a25676
seohyon-cho <flanique@naver.com>
2024-01-23 09:58:18 +0900

    자주 사용하는 style 프리셋 지정

--- components/atoms/text/Text.jsx
+++ components/atoms/text/Text.jsx
@@ -2,14 +2,26 @@ import clsx from 'clsx';
 import styles from './text.module.scss';
 import React from 'react';
 import Link from 'next/link';
+import { Nanum_Myeongjo, Orbitron } from 'next/font/google';
 
-export default function Text({ tagName = 'p', children, url }) {
-	/*
-    [ JSX를 반환하는 리액트 내장 메서드 ]
-      React.createElement(elementType: string, props: object, children: JSX Node)
-  */
-	// return <h1 className={clsx(styles.text)}>{children}</h1>;
+const nanum = Nanum_Myeongjo({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	preload: true,
+	variable: '--font-nanum',
+});
 
-	// url props가 있으면, React.createElement로 Link 컴포넌트로 props 값 설정한 뒤에 children을 감싸서 리턴하도록 하고, 없으면 그냥 children 값만 그대로 리턴.
-	return React.createElement(tagName, { className: clsx(styles.text) }, url ? React.createElement(Link, { href: url }, children) : children);
+const orbitron = Orbitron({
+	subsets: ['latin'],
+	weight: ['400', '600'],
+	preload: true,
+	variable: '--font-orbitron',
+});
+
+export default function Text({ tagName = 'p', children, url, styleType }) {
+	return React.createElement(
+		tagName,
+		{ className: clsx(styles.text, nanum.variable, orbitron.variable, styles[styleType]) },
+		url ? React.createElement(Link, { href: url }, children) : children
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,7 @@ import clsx from 'clsx';
 import styles from './Home.module.scss';
 import Text from '@/components/atoms/text/Text';
 
-// getStaticProps에서 전달받은 props 에서, props에 있는 meals라는 property 자체를 아예 비구조화할당으로 가져옴. = export default function Home(props.meals){};
 export default function Home({ meals }) {
-	// meals 의 데이터 여러 개 중, 5개만 가져오는 것.
-	// 기존, 밑에서 map 돌릴 때 if (idx<=5) return; 으로 설정 갯수 넘어가면 끊어버리는 거랑 같은 역할.
 	const mealsData = meals.slice(0, 5);
 	return (
 		<>
@@ -17,38 +14,23 @@ export default function Home({ meals }) {
 			</Head>
 
 			<main className={clsx(styles.main)}>
-				<h1>Main Page</h1>
-				<Text tagName={'h1'}>Hello</Text>
-				<Text tagName={'span'}>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus dignissimos tempora a labore error, nostrum laboriosam hic odit at earum
-					pariatur deleniti inventore iusto! Sunt quasi pariatur beatae tempore repellat.
+				<Text url={'/'} tagName={'h1'} styleType={'logo'}>
+					DCODELAB
 				</Text>
-				<Text>안녕하세요.</Text>
-				<Text tagName={'h3'} url={'/gallery'}>
-					갤러리로 이동
+				<Text tagName={'h2'} styleType={'title1'}>
+					Title Comes here.
 				</Text>
-				{/* {mealsData.map((item, idx) => {
-					return (
-						<div key={item.idMeal}>
-							<div className={clsx(styles.bg)}>
-								<Pic imgSrc={item.strMealThumb} imgTxt={item.strMeal} url={'/gallery'} />
-							</div>
-						</div>
-					);
-				})} */}
+				<Text>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet inventore pariatur fugiat sapiente dignissimos sequi ipsam fugit et
+					quibusdam illo eligendi illum, suscipit, rerum quisquam neque id. Magni, nesciunt aspernatur.
+				</Text>
+				<Text styleType={'slogan'}>Slogan</Text>
 			</main>
 		</>
 	);
 }
 
-// SSG 방식으로 페이지 렌더링
-// axios.get('데이터를 호출할 URL') (알아서 parsing 까지 해줌.)
-// serialize 관련 에러 뜰 시, 반환 받은 promise 객체를 그대로 내보내는 게 아니라, 실제 data 값까지 추출해낸 뒤에 그 data 값을 props로 전달하면 해결 됨.
-// export async function getStaticProps() {} 자체는 원래 SSG방식으로 렌더링 하는 건데, 여기에 revalidate 라는 옵션이 더해지면 ISR 방식으로 전환되는 개념임.
 export async function getStaticProps() {
 	const { data } = await axios.get('/filter.php?c=Seafood');
-	// {props: }라는 객체로 내보내면, 맨 상단에서 props로 전달 받을 수 있음.
-	// return으로 props 객체 반환하여 컴포넌트로 전달 시, revalidate가 없으면 SSG방식 (한 번 패칭한 데이터를 재패칭하지 않고 static하게 그대로 가지고 가는 것), 있으면 ISR 방식 (revalidate: 로 설정한 시간마다 정기적으로 데이터가 새로 재패칭 되는 것.)
-	// revalidate: @ = @초 단위로 데이터 갱신 (초 단위)
 	return { props: data, revalidate: 60 * 60 * 24 };
 }
