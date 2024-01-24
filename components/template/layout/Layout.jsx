8e0dd94d2c3e70be985e91fd539c21bf987e34b0
seohyon-cho <flanique@naver.com>
2024-01-24 15:55:46 +0900

    페이지가 전환되는 동안 mask 패널 모션 출력 (framer-motion으로 path 값 변경 여부 자체를 trigger로 삼아, 사라지는 모션이 끝날 때까지 해당 컴포넌트의 언마운트 시점을 지연 처리)

--- components/template/layout/Layout.jsx
+++ components/template/layout/Layout.jsx
@@ -4,6 +4,7 @@ import Header from '@/components/organisms/header/Header';
 import Breadcrumb from '@/components/molecules/breadcrumb/Breadcrumb';
 import { Nanum_Myeongjo, Orbitron } from 'next/font/google';
 import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const nanum = Nanum_Myeongjo({
 	subsets: ['latin'],
@@ -22,12 +23,21 @@ const orbitron = Orbitron({
 export default function Layout({ children }) {
 	const router = useRouter();
 	return (
-		<div className={clsx(styles.layout, nanum.variable, orbitron.variable)}>
-			<Header />
-			<div className={clsx(styles.content)}>
-				{router.asPath !== '/' && <Breadcrumb />}
-				{children}
-			</div>
-		</div>
+		// <AnimatePresence> 에 mode='wait' 속성을 부여하면, 기존 컴포넌트에서 실행되고 있는 모션 컴포넌트가 있을 때, 해당 모션이 끝날 때까지 unmount를 지연 시킴. (=> 사라지는 모션까지 온전하게 나타날 수 있도록 모션 시간 확보가 가능해짐. )
+		<AnimatePresence mode='wait'>
+			{/* router 변경을 감지하는 모션 컴포넌트 */}
+			<motion.div key={router.pathname}>
+				<div className={clsx(styles.layout, nanum.variable, orbitron.variable)}>
+					<Header />
+					<div className={clsx(styles.content)}>
+						{router.asPath !== '/' && <Breadcrumb />}
+						{children}
+					</div>
+				</div>
+				{/* router 변경 시마다 실제로 모션이 일어날 박스 요소 */}
+				<motion.div className='in' initial={{ scaleX: 0 }} animate={{ scaleX: 0 }} exit={{ scaleX: 1 }} transition={{ duration: 0.7 }}></motion.div>
+				<motion.div className='out' initial={{ scaleX: 1 }} animate={{ scaleX: 0 }} exit={{ scaleX: 0 }} transition={{ duration: 0.7 }}></motion.div>
+			</motion.div>
+		</AnimatePresence>
 	);
 }
