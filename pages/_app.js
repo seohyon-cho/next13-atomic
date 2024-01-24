ea98e1f76b52379f4a42300d5308ff2c37e806e7
seohyon-cho <flanique@naver.com>
2024-01-24 12:45:08 +0900

    _app.js 루트 컴포넌트 설정완료 (전역데이터, QueryClient 연동, axios 기본 url, ...) : next13 react 기본 셋팅.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import axios from 'axios';
 import '@/styles/globals.scss';
+import '@/styles/theme.scss'; // optional
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GlobalProvider } from '@/hooks/useGlobalContext';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }) {
 	return (
-		<div>
-			<Component {...pageProps} />
-		</div>
+		<GlobalProvider>
+			<QueryClientProvider client={queryClient}>
+				<div>
+					<Component {...pageProps} />
+				</div>
+				<ReactQueryDevtools />
+			</QueryClientProvider>
+		</GlobalProvider>
 	);
 }
 
