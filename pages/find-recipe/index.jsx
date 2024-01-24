e8464055cc2f68ab6039bef1fe121b08456472da
seohyon-cho <flanique@naver.com>
2024-01-24 17:07:14 +0900

    카테고리별로 연동된 데이터 목록 출력

--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -3,11 +3,16 @@ import axios from 'axios';
 import styles from './find-recipe.module.scss';
 import Category from '@/components/molecules/category/Category';
 import { useState } from 'react';
+import { useRecipeByCategory } from '@/hooks/useRecipe';
+import Card from '@/components/molecules/card/Card';
 
 export default function FindRecipe({ categories }) {
 	const [Names, setNames] = useState(categories.map(el => el.strCategory));
 	const [Selected, setSelected] = useState(categories[0].strCategory);
-	console.log(Selected);
+
+	// 선택한 카테고리명인 Selected 를 인수로 넣음.
+	const { data: dataByCategory, isSuccess } = useRecipeByCategory(Selected, '');
+	console.log(dataByCategory);
 
 	const handleClick = activeEl => {
 		setSelected(activeEl);
@@ -17,6 +22,11 @@ export default function FindRecipe({ categories }) {
 		<section className={clsx(styles.findRecipe)}>
 			<h1>Find Recipe</h1>
 			<Category dataArr={Names} selectedEl={Selected} onClick={handleClick} className={clsx(styles.category)} />
+			<h2>{Selected}</h2>
+			{isSuccess &&
+				dataByCategory.map(data => {
+					return <Card key={data.idMeal} imgSrc={data.strMealThumb} txt={data.strMeal} className={clsx(styles.foodItem)} />;
+				})}
 		</section>
 	);
 }
