055a837357ba716a529ba9f02ed161aa16be572b
seohyon-cho <flanique@naver.com>
2024-01-24 15:33:17 +0900

    현재 path 경로가 메인페이지일 때에 한해 breadcrumb 숨김처리

--- components/template/layout/Layout.jsx
+++ components/template/layout/Layout.jsx
@@ -3,6 +3,7 @@ import styles from './layout.module.scss';
 import Header from '@/components/organisms/header/Header';
 import Breadcrumb from '@/components/molecules/breadcrumb/Breadcrumb';
 import { Nanum_Myeongjo, Orbitron } from 'next/font/google';
+import { useRouter } from 'next/router';
 
 const nanum = Nanum_Myeongjo({
 	subsets: ['latin'],
@@ -19,11 +20,12 @@ const orbitron = Orbitron({
 });
 
 export default function Layout({ children }) {
+	const router = useRouter();
 	return (
 		<div className={clsx(styles.layout, nanum.variable, orbitron.variable)}>
 			<Header />
 			<div className={clsx(styles.content)}>
-				<Breadcrumb />
+				{router.asPath !== '/' && <Breadcrumb />}
 				{children}
 			</div>
 		</div>
