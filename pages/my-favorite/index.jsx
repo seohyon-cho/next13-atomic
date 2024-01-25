16a921ac09d0fda4dc00c7c1a99b067cc1762945
seohyon-cho <flanique@naver.com>
2024-01-25 16:00:05 +0900

    즐겨찾기 설정한 데이터 My favorite 페이지에서 목록으로 출력

--- pages/my-favorite/index.jsx
+++ pages/my-favorite/index.jsx
@@ -1,10 +1,36 @@
 import clsx from 'clsx';
 import styles from './my-favorite.module.scss';
+import { useEffect, useState } from 'react';
+import { useRecipesByIds } from '@/hooks/useRecipe';
+import Card from '@/components/molecules/card/Card';
 
 export default function MyFavorite() {
+	const [SavedId, setSavedId] = useState([]);
+
+	useEffect(() => {
+		setSavedId(JSON.parse(localStorage.getItem('favorite')) || []);
+	}, []);
+
+	const result = useRecipesByIds(SavedId);
+	console.log(result);
+
 	return (
 		<section className={clsx(styles.myFavorite)}>
 			<h1>My favorite</h1>
+
+			{result.map(({ data, isSuccess }) => {
+				if (isSuccess) {
+					return (
+						<Card
+							key={data.idMeal}
+							imgSrc={data.strMealThumb}
+							txt={data.strMeal}
+							className={clsx(styles.favoriteCard)}
+							url={`/find-recipe/${data.idMeal}?name=${data.strMeal}`}
+						/>
+					);
+				}
+			})}
 		</section>
 	);
 }
