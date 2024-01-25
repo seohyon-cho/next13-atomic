f1f8300b95b538476c988de0cfba4926f0fd5536
seohyon-cho <flanique@naver.com>
2024-01-25 12:37:35 +0900

    검색어를 입력할 때마다, 해당 검색 키워드로 실시간 데이터 fetching 및 목록 출력

--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -3,17 +3,19 @@ import axios from 'axios';
 import styles from './find-recipe.module.scss';
 import Category from '@/components/molecules/category/Category';
 import { useState } from 'react';
-import { useRecipeByCategory } from '@/hooks/useRecipe';
+import { useRecipeByCategory, useRecipeBySearch } from '@/hooks/useRecipe';
 import Card from '@/components/molecules/card/Card';
 import SearchBar from '@/components/molecules/searchBar/SearchBar';
 
 export default function FindRecipe({ categories }) {
 	const [Names, setNames] = useState(categories.map(el => el.strCategory));
 	const [Selected, setSelected] = useState(categories[0].strCategory);
+	const [Search, setSearch] = useState('');
+	console.log(Search);
 
 	// 선택한 카테고리명인 Selected 를 인수로 넣음.
 	const { data: dataByCategory, isSuccess } = useRecipeByCategory(Selected, '');
-	console.log(dataByCategory);
+	const { data: dataBySearch, isSuccess: isSearch } = useRecipeBySearch(Search);
 
 	const handleClick = activeEl => {
 		setSelected(activeEl);
@@ -23,10 +25,22 @@ export default function FindRecipe({ categories }) {
 		<section className={clsx(styles.findRecipe)}>
 			<div className={clsx(styles.controller)}>
 				<Category dataArr={Names} selectedEl={Selected} onClick={handleClick} className={clsx(styles.category)} />
-				<SearchBar placeholder={'Search Recipe'} className={clsx(styles.searchBox)} />
+				<SearchBar value={Search} onChange={setSearch} placeholder={'Search Recipe'} className={clsx(styles.searchBox)} />
 			</div>
 
 			<h1>{Selected}</h1>
+			{isSearch &&
+				dataBySearch.map(data => {
+					return (
+						<Card
+							key={data.idMeal}
+							imgSrc={data.strMealThumb}
+							txt={data.strMeal}
+							className={clsx(styles.foodItem)}
+							url={`/find-recipe/${data.idMeal}?name=${data.strMeal}`}
+						/>
+					);
+				})}
 			{isSuccess &&
 				dataByCategory.map(data => {
 					return (
