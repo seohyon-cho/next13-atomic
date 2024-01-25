96b15e1873a8ce1f022a4079546ffc94a5c73d63
seohyon-cho <flanique@naver.com>
2024-01-25 16:35:11 +0900

    페이지 컴포넌트단에서 style 적용에 우선순위를 부여(덮어쓰기) 하기 위해서는 해당 페이지 전용의 style scss를 가장 아래에 import해서 덮어쓰기 되도록 처리

--- pages/my-favorite/index.jsx
+++ pages/my-favorite/index.jsx
@@ -1,8 +1,10 @@
 import clsx from 'clsx';
+import Card from '@/components/molecules/card/Card';
 import styles from './my-favorite.module.scss';
+// 동등한 구조의 스타일을 덮어쓰기 할 때, page 컴포넌트의 스타일이 가장 높은 우선순위를 가져야하기 때문에, import 구문에서 page 전용 스타일이 가장 최종적으로 연결되어야 함. (여기서는 ./my-favorite.module.scss)
+// 위의 순서처럼 스타일 파일을 import 해야, Card 컴포넌트에 연결되어 있는 스타일이 먼저 적용되고, 그 뒤에 페이지 전용 스타일로 한 번 더 덮어쓰기가 됨.
 import { useEffect, useState } from 'react';
 import { useRecipesByIds } from '@/hooks/useRecipe';
-import Card from '@/components/molecules/card/Card';
 
 export default function MyFavorite() {
 	const [SavedId, setSavedId] = useState([]);
